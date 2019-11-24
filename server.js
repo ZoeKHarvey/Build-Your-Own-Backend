01a1063975d643d598a4a40e6e62f085f61cedf4
Zoe <zoekharvey@gmail.com>
2019-11-24 13:13:53 -0700

    Add first get, broke album seeding

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-console.log('configuration', configuration)
 const database = require('knex')(configuration)
-console.log('database', database)
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/api/v1/footnotes', (req, res) => {
-  database('footnotes').select().then(footnotes => {
-    res.status(200).json(footnotes)
+app.get('/api/v1/albums', (request, response) => {
+  database('albums').select()
+  .then((albums) => {
+    response.status(200).json(albums)
+  })
+  .catch((error) => {
+    response.status(500).json({error})
   })
 })
 
