348ee808c5945ec2c9b77b73eebfee2e201a54cb
Zoe <zoekharvey@gmail.com>
2019-11-24 18:16:44 -0700

    Update for deployment

--- knexfile.js
+++ knexfile.js
@@ -24,13 +24,4 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
 };
