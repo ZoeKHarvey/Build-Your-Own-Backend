4d418a8ee1bc8516c1c022b944a65293a86c88f6
Zoe <zoekharvey@gmail.com>
2019-11-24 20:14:09 -0700

    Start articulation requirements with set up and first album get

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+// Tells server to use http / allows use of http requests/bodies/headers
 const express = require('express');
+// imports Express
 const app = express();
+// Extracts entire body portion of incoming request and exposes it on req.body / parses the JSON, string and URL encoded data submitted by the POST request) 
 const bodyParser = require('body-parser')
 
+//Tells our app what environment it's in, it's default will be 'development'
 const environment = process.env.NODE_ENV || 'development';
+// Configures knexfile based on given environment
 const configuration = require('./knexfile')[environment];
+// What knex uses to configure our database (based on environment)
 const database = require('knex')(configuration)
 
+//Sets port of server with environment. If it can't find an environtment it's set to 3000
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json())
 
+// Gives the endpoint for a GET request
 app.get('/api/v1/albums', (request, response) => {
+  // queries the 'albums' table, selects each row in table
   database('albums').select()
   .then((albums) => {
+    // it will now JSON the fetched data, send it with a successful status code of 200
     response.status(200).json(albums)
   })
+  // if there's an error, it'll send an internal server error response with an error code of 500
   .catch((error) => {
     response.status(500).json({ error })
   })
 })
+
 
 app.get('/api/v1/songs', (request, response) => {
   database('songs').select()
